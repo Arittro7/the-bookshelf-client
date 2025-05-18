48804095577995522e78475858ca2fb7788ee9d2
Arittro7 <arittrowpdev@gmail.com>
2025-05-18 16:55:08 +0600

    Navbar

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,24 @@
-import errGif from "../../assets/errorPage.gif"
+import { Link } from "react-router-dom";
+import errGif from "../../assets/errorPage.gif";
+import { GiBookshelf } from "react-icons/gi";
 const ErrorPage = () => {
   return (
     <div>
-      <div className="text-5xl text-center font-bold mt-28">
-        <h1>Oops! Something went wrong.</h1>
-      <p className="text-7xl mt-6">404</p>
+      <div className="absolute w-full z-10 text-center font-bold mt-6">
+        <h1 className="text-2xl md:text-5xl">Oops! Something went wrong.</h1>
+        <p className="text-7xl mt-6 font-bold">404</p>
       </div>
-      <div className="w-screen h-screen -mt-10 flex justify-center items-center">
-      <img src={errGif} alt="" />
-    </div>
+      <div className="relative w-screen  h-screen flex flex-col justify-center items-center">
+        <img src={errGif} alt="" />
+        <div className="absolute bottom-12">
+          <div className=" bg-yellow-400 py-2 px-4 rounded-lg ">
+            <Link className="flex gap-2 justify-center items-center" to={"/"}>
+            <p className="uppercase font-semibold">Back to Shelf</p>
+            <GiBookshelf />
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,23 +1,76 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png"
 import { IoSearchOutline } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
+import { FiBookmark } from "react-icons/fi";
+import { RiBookShelfFill } from "react-icons/ri";
+import user from '../../assets/user.png'
+import { useState } from "react";
+
+const navbarDropdown = [
+  {name: "Dashboard", Link: "/dashboard"},
+  {name: "Orders", Link: "/order"},
+  {name: "Cart", Link: "/cart"},
+  {name: "Check Out", Link: "/checkout"},
+]
+
 const Navbar = () => {
+
+  const currentUser = true;
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
   return (
-    <div className="w-full bg-yellow-400 mb-1.5">
+    <div className="w-full mb-1.5">
       <div className="px-6 flex justify-between py-2 h-16 items-center">
         {/* left side */}
-        <div className="flex gap-4">
+        <div className=" flex gap-4 ">
           <Link to="/">
           <img className="size-14 rounded-full" src={logo} alt="" />
           </Link>
-          <div className="flex items-center gap-2">
-            <IoSearchOutline/>
-            <input type="text" name="" className="bg-[#EAEAEA] w-full" placeholder="Search here..." />
+          <div className=" relative flex items-center gap-2">
+            <IoSearchOutline className="absolute left-2"/>
+            <input type="text" name="" className="bg-[#EAEAEA] md:w-full w-[122px] px-8 py-1 rounded-lg" placeholder="Search here..." />
           </div>
         </div>
         {/* right side */}
-        <div> 
-          <h1>Right</h1>
+        <div className="flex gap-3 items-center"> 
+          <button className="hidden sm:block">
+            <FiBookmark className="size-6"/>
+          </button>
+          <div>
+            {
+              currentUser ? <>
+              <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                <img src={user} className={`size-6 ${currentUser ? 'ring-2 ring-yellow-400 rounded-full mt-1.5' : ''} `}/>
+              </button> 
+              {/* dropdown */}
+              {
+                isDropdownOpen && (
+                  <div>
+                    <ul>
+                      {
+                        navbarDropdown.map((item) =>(
+                         <li key={item.name}>
+                          <Link to={item.Link}>
+                          {item.name}
+                          </Link>
+                         </li> 
+                        ))
+                      }
+                    </ul>
+                  </div>
+                )
+              }
+              </> : 
+              <Link to={'/login'}><FaUser className="size-6"/></Link>
+            }
+          </div>
+          
+          <Link to="/cart" className="flex gap-2 items-center rounded-md bg-yellow-400 md:px-6 md:py-1 py-1 px-2">
+          <RiBookShelfFill className="size-7"/>
+          <span className="text-xl">0</span>
+          </Link>
         </div>
       </div>
     </div>
